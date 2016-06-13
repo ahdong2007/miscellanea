c0944e3127471b937928f0dc12fb5e8bcb93dc18
Mason Hua <ahdong2007@hotmail.com>
2016-06-13 09:40:11 +0800

    add

--- fix_vio.sh
+++ fix_vio.sh
@@ -21,19 +21,37 @@ check_pwd_strategy() {
 }
 
 check_no_passwd() {
+
+        flag=0
+
 	for user in $(awk -F: '($2 == "") {print $1}' /etc/shadow)
 	do
 		echo_failure "user $user did't setup a password"
+                flag=1
 	done
+
+        if [ $flag -eq 0 ]; then
+                echo_ok "No user has empty passwd"
+        fi
 }
 
 set_users() {
 
+
+        chattr -i /etc/passwd
+        chattr -i /etc/shadow
+        chattr -i /etc/group
+        chattr -i /etc/gshadow
+
 	# games, ftp
-	for user in "adm, lp, sync, shutdown, halt, news, uucp, operator, gopher"
+	for user in $(echo -e "adm\nlp\nsync\nshutdown\nhalt\nnews\nuucp\noperator\ngopher")
 	do
 		# userdel $user
-		chsh -s /sbin/nologin
+                id $user >/dev/null 2>&1
+                if [ $? -eq 0 ]; then
+		      chsh -s /sbin/nologin $user >/dev/null 2>&1
+                      echo_ok "Changed shell to /sbin/nologin for $user"
+                fi
 	done
 
 	chattr +i /etc/passwd
@@ -45,10 +63,15 @@ set_users() {
 
 set_home() {
 
-	for dir in $(awk -F: '($3 >= 500) {print $6}' /etc/passwd)
+	for attr in $(awk -F: 'BEGIN{OFS=","} ($3 >= 500) {print $1,$6}' /etc/passwd)
 	do
+                dir=$(echo $attr | cut -d, -f2)
+                user=$(echo $attr | cut -d, -f1)
+
 		chmod g-rwx $dir
 		chmod o-rwx $dir
+                echo_ok "Changed for user $user: chmod g-rwx $dir"
+                echo_ok "Changed for user $user: chmod o-rwx $dir"
 	done
 }
 
@@ -74,12 +97,20 @@ set_selinux() {
 }
 
 check_root_user() {
+
+        flag=0
+
 	for user in $(awk -F: '($3 == 0) { print $1 }' /etc/passwd)
 	do
 		if [ "$user" != "root" ]; then
-			echo_failure "user $user can't have UID=0"
+			echo_failure "user $user can't has UID=0"
+                        flag=1
 		fi
 	done
+
+        if [  $flag -eq 0 ]; then
+                echo_ok "Only root has uid=0"
+        fi
 }
 
 
@@ -92,18 +123,19 @@ do
         clear
         num=1
         echo -e "\r\r\r\r\r"
-        echo -e "\t\t\t===================="
+        echo -e "\t\t\t ===================="
         echo -e "\t\t\t Linux系统安全管理菜单"
-        echo -e "\t\t\t===================="
+        echo -e "\t\t\t ==================="
         echo ""
-        echo -e "\t\t\ta.  执行下面1-11步"
+        echo -e "\t\t\ta.  执行下面1-6步"
         echo ""
         echo -e "\t\t\t1).  检查密码安全策略"
         echo -e "\t\t\t2).  检查密码为空的用户"
         echo -e "\t\t\t3).  检查uid=0的用户"
         echo -e "\t\t\t4).  清除不必要的系统账户"
         echo -e "\t\t\t5).  检查用户主目录权限"
-        echo -e "\t\t\t6).  用户umask值检查"
+        echo -e "\t\t\t6).  设置SELinux"
+        echo -e "\t\t\t7).  用户umask值检查"
         echo ""
         echo -e "\t\t\tQ.  Quit"
         echo ""
@@ -142,6 +174,13 @@ do
                 5)      clear
                         echo -e "\n\n\n\n\n\n\n"
                         set_home 5
+                        echo "Press enter to continue..."
+                        read qqq
+                        continue
+                        ;;
+                6)      clear
+                        echo -e "\n\n\n\n\n\n\n"
+                        set_selinux 6
                         echo "Press enter to continue..."
                         read qqq
                         continue
