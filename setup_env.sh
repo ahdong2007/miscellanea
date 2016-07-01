c0686b1fad5d9cf422f79b036544193490528f0d
ahdong2007 <ahdong2007@hotmail.com>
2016-07-01 13:22:27 +0800

    update

--- setup_env.sh
+++ setup_env.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 
 #
-# echo the status with same column size
+# 信息技术中心 平台组 华远东
 #
+# 日期        变更人        变更          
+# 2016/07/01  华远东        初始版本
+#
+#
+
 echo_ok() {
-  COL_SIZE=60
+  COL_SIZE=80
   STRING="$1"
   echo -e "${STRING} \\033[${COL_SIZE}G [\\033[1;32m ok \\033[0;39m]"
 }
 
 echo_failure() {
-  COL_SIZE=60
+  COL_SIZE=80
   STRING="$1"
   echo -e "${STRING} \\033[${COL_SIZE}G [\\033[1;31mFAILED\\033[0;39m]"
 }
+
+# run it with root
+if [ $(id -u) -ne 0 ]; then
+    echo_failure "run it with root only"
+    exit 1
+fi
+
+# close std output and error
+exec 1>/dev/null
+exec 2>/dev/null
 
 disable_usb() {
     cfgfile='/etc/modprobe.d/usb-storage.conf'
@@ -38,6 +53,21 @@ pwd_strategy() {
     # make copy first
     \cp $cfgfile ${cfgfile}.bak
     sed -i "s/password    requisite     pam_cracklib.so.*/password    required      pam_cracklib.so try_first_pass retry=6 minlen=8 dcredit=-1 ucredit=-1 ocredit=-1 lcredit=-1 enforce_for_root/g" $cfgfile
+}
+
+pwd_life_time() {
+
+    chattr -i /etc/passwd
+    chattr -i /etc/shadow
+    chattr -i /etc/group
+    chattr -i /etc/gshadow
+
+    chage -d 0 -m 0 -M 90 -W 7 root
+
+    chattr +i /etc/passwd
+    chattr +i /etc/shadow
+    chattr +i /etc/group
+    chattr +i /etc/gshadow
 }
 
 disable_users() {
@@ -116,7 +146,7 @@ disable_services() {
         service $service stop
         chkconfig $sevice off
     done
-}
+} > /dev/null 2>&1
 
 set_selinux() {
 
@@ -148,3 +178,37 @@ check_no_passwd() {
 
 
 # main function
+exec 1<&0
+exec 2<&0
+
+disable_usb
+echo_ok "task 1: disable usb-storage"
+
+disable_control_alt_delete
+echo_ok "task 2: disable Control+Alt+Delete reboot server"
+
+pwd_strategy
+echo_ok "task 3: setup password strategy"
+
+pwd_life_time
+echo_ok "task 4: setup password life time to 90 days"
+
+disable_users
+echo_ok "task 5: disable useless system usrss"
+
+check_root_user
+echo_ok "task 6: check uid=0 users"
+
+setup_login_defs
+echo_ok "task 7: setup login configurations"
+
+setup_profile
+echo_ok "task 8: setup global profile"
+
+disable_services
+echo_ok "task 9: stop unnecessary services"
+
+set_selinux
+echo_ok "task 10: setup SELinux"
+
+echo "end of task"
