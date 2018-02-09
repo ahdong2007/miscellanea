50b0329eb64378e0621a2128894b3ac095f7988a
ahdong2007 <ahdong2007@hotmail.com>
2018-02-09 15:51:07 +0800

    增加注释

--- functions.sh
+++ functions.sh
@@ -1,21 +1,31 @@
 
+# 对一个目录进行递归搜索和替换
+# 比如：sr wrong_word correct_word
 function sr {
     find . -type f -exec sed -i '' s/$1/$2/g {} +
 }
 
+
+# 格式化输出里取某一列
+# 比如：git status -s | col 2
 function col {
   awk -v col=$1 '{print $col}'
 }
 
+
+# 忽略前n个词
+# docker rmi $(docker images | col 3 | xargs | skip 1)
 function skip {
     n=$(($1 + 1))
     cut -d' ' -f$n-
 }
 
+
 function get_dir() {
 
 	DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 }
+
 
 function get_dir_with_link() {
 
@@ -28,6 +38,3 @@ function get_dir_with_link() {
 	DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 	echo $DIR
 }
-
-
-get_dir_with_link
